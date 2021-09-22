42899d3f613a5193b8190a3d26545600e40bee1f
Vechkanova <vechkanova.p@gmail.com>
2021-09-22 15:09:08 +0400

    Покрытие класса Person тестами

---
+++ src/main/java/ru/ssau/tk/name/practice/Point.java
@@ -0,0 +1,14 @@
+package ru.ssau.tk.name.practice;
+
+public class Point {
+    public final double x;
+    public final double y;
+    public final double z;
+    public Point(double x, double y, double z){
+        this.x=x;
+        this.y=y;
+        this.z=z;
+    }
+    Point first = new Point(1,2,3);
+    Point second = new Point(2,5,1);
+}
