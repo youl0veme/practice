666be0fee0cc742374ba69c2be12df173d7b31e8
Vechkanova <vechkanova.p@gmail.com>
2021-10-07 13:46:12 +0400

    Перенесение метода main() в отдельный класс

---
+++ src/main/java/ru/ssau/tk/name/practice/MainPerson.java
@@ -0,0 +1,10 @@
+package ru.ssau.tk.name.practice;
+
+public class MainPerson {
+    public static void main(String[] args) {
+        Person p1 = new Person("Green", "Nick", 7654321);
+        Person p2 = new Person("White", "Bob");
+        Person p3 = new Person(123456);
+        Person p4 = new Person();
+    }
+}
