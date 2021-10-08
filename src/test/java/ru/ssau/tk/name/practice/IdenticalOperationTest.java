358e5d25bbca99e055f5d1c4b68006f886a49540
Vechkanova <vechkanova.p@gmail.com>
2021-10-08 17:30:56 +0400

    Добавление теста testApplyTriple() для IdenticalOperation

--- src/test/java/ru/ssau/tk/name/practice/IdenticalOperationTest.java
+++ src/test/java/ru/ssau/tk/name/practice/IdenticalOperationTest.java
@@ -3,9 +3,10 @@ package ru.ssau.tk.name.practice;
 import org.testng.annotations.*;
 
 import static org.testng.Assert.*;
+import static ru.ssau.tk.name.practice.Operation.*;
 
 public class IdenticalOperationTest {
-    private static final double DELTA = 0.001;
+
     IdenticalOperation identical = new IdenticalOperation();
 
     @Test
@@ -13,6 +14,14 @@ public class IdenticalOperationTest {
         assertEquals(identical.apply(1), 1, DELTA);
         assertEquals(identical.apply(Double.NEGATIVE_INFINITY), Double.NEGATIVE_INFINITY, DELTA);
         assertEquals(identical.apply(Double.POSITIVE_INFINITY), Double.POSITIVE_INFINITY, DELTA);
-        assertEquals(identical.apply(Double.NaN), Double.NaN,DELTA);
+        assertEquals(identical.apply(Double.NaN), Double.NaN, DELTA);
+    }
+
+    @Test
+    public void testApplyTriple(){
+        assertEquals(identical.applyTriple(3),3,DELTA);
+        assertEquals(identical.applyTriple(Double.NaN), Double.NaN,DELTA);
+        assertEquals(identical.applyTriple(Double.NEGATIVE_INFINITY), Double.NEGATIVE_INFINITY,DELTA);
+        assertEquals(identical.applyTriple(Double.POSITIVE_INFINITY),Double.POSITIVE_INFINITY,DELTA);
     }
 }
