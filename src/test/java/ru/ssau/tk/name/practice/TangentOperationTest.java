8154d09634e036a0853d9a2bc5a36e68576e1ed5
Vechkanova <vechkanova.p@gmail.com>
2021-10-08 17:51:10 +0400

    Добавление тестов TangentOperationTest

---
+++ src/test/java/ru/ssau/tk/name/practice/TangentOperationTest.java
@@ -0,0 +1,26 @@
+package ru.ssau.tk.name.practice;
+
+import org.testng.annotations.*;
+
+import static org.testng.Assert.*;
+import static ru.ssau.tk.name.practice.Operation.DELTA;
+
+public class TangentOperationTest {
+    TangentOperation tan = new TangentOperation();
+
+    @Test
+    public void testApply() {
+        assertEquals(tan.apply(Math.PI / 4), 1, DELTA);
+        assertEquals(tan.apply(Double.NaN), Double.NaN, DELTA);
+        assertEquals(tan.apply(Double.NEGATIVE_INFINITY), Double.NaN, DELTA);
+        assertEquals(tan.apply(Double.POSITIVE_INFINITY), Double.NaN, DELTA);
+    }
+
+    @Test
+    public void testTriple() {
+        assertEquals(tan.applyTriple(Math.PI / 4), Math.tan(Math.tan(1)), DELTA);
+        assertEquals(tan.applyTriple(Double.NaN), Double.NaN, DELTA);
+        assertEquals(tan.applyTriple(Double.NEGATIVE_INFINITY), Double.NaN, DELTA);
+        assertEquals(tan.applyTriple(Double.POSITIVE_INFINITY), Double.NaN, DELTA);
+    }
+}
