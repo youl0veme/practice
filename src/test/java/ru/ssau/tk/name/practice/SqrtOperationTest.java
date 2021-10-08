19bf27215c54d32bf03d3ba7283f2814d279dcd1
Vechkanova <vechkanova.p@gmail.com>
2021-10-08 17:39:11 +0400

    Добавление тестов SqrtOperationTest

---
+++ src/test/java/ru/ssau/tk/name/practice/SqrtOperationTest.java
@@ -0,0 +1,26 @@
+package ru.ssau.tk.name.practice;
+
+import org.testng.annotations.*;
+
+import static org.testng.Assert.*;
+import static ru.ssau.tk.name.practice.Operation.*;
+
+public class SqrtOperationTest {
+    SqrtOperation sqrt = new SqrtOperation();
+
+    @Test
+    public void testApply() {
+        assertEquals(sqrt.apply(4), 2, DELTA);
+        assertEquals(sqrt.apply(Double.POSITIVE_INFINITY), Double.POSITIVE_INFINITY, DELTA);
+        assertEquals(sqrt.apply(Double.NEGATIVE_INFINITY), Double.NaN, DELTA);
+        assertEquals(sqrt.apply(Double.NaN), Double.NaN, DELTA);
+    }
+
+    @Test
+    public void testApplyTriple() {
+        assertEquals(sqrt.applyTriple(256), 2, DELTA);
+        assertEquals(sqrt.applyTriple(Double.POSITIVE_INFINITY), Double.POSITIVE_INFINITY, DELTA);
+        assertEquals(sqrt.applyTriple(Double.NEGATIVE_INFINITY), Double.NaN, DELTA);
+        assertEquals(sqrt.applyTriple(Double.NaN), Double.NaN, DELTA);
+    }
+}
