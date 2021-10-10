696e2711e3aae2eecf54630fd057091e90cb3a22
Vechkanova <vechkanova.p@gmail.com>
2021-10-10 11:33:37 +0400

    Залдание 1.1 Добавлен класс, его методы и тесты

---
+++ src/main/java/ru/ssau/tk/name/practice/PrintType.java
@@ -0,0 +1,46 @@
+package ru.ssau.tk.name.practice;
+
+public class PrintType {
+
+    public static void printType(byte temp) {
+        System.out.println("byte");
+    }
+
+    public static void printType(char temp) {
+        System.out.println("char");
+    }
+
+    public static void printType(short temp) {
+        System.out.println("short");
+    }
+
+    public static void printType(int temp) {
+        System.out.println("int");
+    }
+
+    public static void printType(long temp) {
+        System.out.println("long");
+    }
+
+    public static void printType(float temp) {
+        System.out.println("float");
+    }
+
+    public static void printType(double temp) {
+        System.out.println("double");
+    }
+
+    public static void printType(boolean temp) {
+        System.out.println("boolean");
+    }
+
+    public static void printType(Object temp) {
+        if (temp == null) {
+            System.out.println("null");
+        } else {
+            System.out.println(temp.getClass());
+        }
+    }
+}
+
+
---
+++ src/test/java/ru/ssau/tk/name/practice/PrintTypeTest.java
@@ -0,0 +1,21 @@
+package ru.ssau.tk.name.practice;
+
+import org.testng.annotations.*;
+
+public class PrintTypeTest {
+    @Test
+    public void testPrintType() {
+        PrintType.printType("temp");
+        PrintType.printType('w');
+        PrintType.printType((byte) 1);
+        PrintType.printType((short) 1);
+        PrintType.printType(1.);
+        PrintType.printType(22L);
+        PrintType.printType(6.6f);
+        PrintType.printType(1);
+        PrintType.printType(true);
+        PrintType.printType(new Person());
+        PrintType.printType(new Point(0, 0, 0));
+        PrintType.printType(null);
+    }
+}
