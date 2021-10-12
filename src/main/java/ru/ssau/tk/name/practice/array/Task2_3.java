48c150462ded1b2d67645a8d00508d0de8cd5bbe
Vechkanova <vechkanova.p@gmail.com>
2021-10-12 18:24:43 +0400

    Форматирование

--- src/main/java/ru/ssau/tk/name/practice/array/Task2_3.java
+++ src/main/java/ru/ssau/tk/name/practice/array/Task2_3.java
@@ -1,10 +1,10 @@
 package ru.ssau.tk.name.practice.array;
 
 public class Task2_3 {
-    public static int[] generateArray(int size){
+    public static int[] generateArray(int size) {
         int[] array = new int[size];
-        for(int i=0; i<array.length; i++){
-           array[i]=2*i+1;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = 2 * i + 1;
         }
         return array;
     }
--- src/test/java/ru/ssau/tk/name/practice/array/Task2_3Test.java
+++ src/test/java/ru/ssau/tk/name/practice/array/Task2_3Test.java
@@ -9,9 +9,9 @@ public class Task2_3Test {
     public void testGenerateArray() {
         int[] array = Task2_3.generateArray(4);
         assertEquals(array.length, 4);
-        assertEquals(array[0],1);
+        assertEquals(array[0], 1);
         assertEquals(array[1], 3);
-        assertEquals(array[2],5);
+        assertEquals(array[2], 5);
         assertEquals(array[3], 7);
     }
 }
