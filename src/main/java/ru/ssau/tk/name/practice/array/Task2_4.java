499ba7d91e6141608e9c4bdde5c385c3866a7c8f
Vechkanova <vechkanova.p@gmail.com>
2021-10-12 19:01:35 +0400

    Задание 2.4

--- src/main/java/ru/ssau/tk/name/practice/array/Task2_4.java
+++ src/main/java/ru/ssau/tk/name/practice/array/Task2_4.java
@@ -1,10 +1,10 @@
 package ru.ssau.tk.name.practice.array;
 
 public class Task2_4 {
-    public static int[] generateArray(int size){
+    public static int[] generateArray(int size) {
         int[] array = new int[size];
-        for(int i=0; i< array.length;i++){
-            array[i]=(size-i)*2;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (size - i) * 2;
         }
         return array;
     }
--- src/test/java/ru/ssau/tk/name/practice/array/Task2_4Test.java
+++ src/test/java/ru/ssau/tk/name/practice/array/Task2_4Test.java
@@ -1,15 +1,18 @@
 package ru.ssau.tk.name.practice.array;
+
 import org.testng.annotations.*;
+
 import static org.testng.Assert.*;
+
 public class Task2_4Test {
     @Test
-    public void testGenerateArray(){
+    public void testGenerateArray() {
         int[] array = Task2_4.generateArray(5);
-        assertEquals(array.length,5);
-        assertEquals(array[4],2);
-        assertEquals(array[3],4);
-        assertEquals(array[2],6);
+        assertEquals(array.length, 5);
+        assertEquals(array[4], 2);
+        assertEquals(array[3], 4);
+        assertEquals(array[2], 6);
         assertEquals(array[1], 8);
-        assertEquals(array[0],10);
+        assertEquals(array[0], 10);
     }
 }
