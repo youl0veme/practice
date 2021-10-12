47bc64762e17f94e79cd01f71a15036d75ea9859
Vechkanova <vechkanova.p@gmail.com>
2021-10-12 18:24:11 +0400

    Задание 2.3 (Создан метод и тест к нему)

---
+++ src/main/java/ru/ssau/tk/name/practice/array/Task2_3.java
@@ -0,0 +1,11 @@
+package ru.ssau.tk.name.practice.array;
+
+public class Task2_3 {
+    public static int[] generateArray(int size){
+        int[] array = new int[size];
+        for(int i=0; i<array.length; i++){
+           array[i]=2*i+1;
+        }
+        return array;
+    }
+}
---
+++ src/test/java/ru/ssau/tk/name/practice/array/Task2_3Test.java
@@ -0,0 +1,17 @@
+package ru.ssau.tk.name.practice.array;
+
+import static org.testng.Assert.*;
+
+import org.testng.annotations.*;
+
+public class Task2_3Test {
+    @Test
+    public void testGenerateArray() {
+        int[] array = Task2_3.generateArray(4);
+        assertEquals(array.length, 4);
+        assertEquals(array[0],1);
+        assertEquals(array[1], 3);
+        assertEquals(array[2],5);
+        assertEquals(array[3], 7);
+    }
+}
