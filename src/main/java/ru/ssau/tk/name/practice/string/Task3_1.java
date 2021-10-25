d739159f31f875a52b0ea9d557c6301c4c1b2078
Vechkanova <vechkanova.p@gmail.com>
2021-10-25 13:20:52 +0400

    Задание 3.1

---
+++ src/main/java/ru/ssau/tk/name/practice/string/Task3_1.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.name.practice.string;
+
+public class Task3_1 {
+    public static void printSymbols(String s) {
+        for (int i = 0; i < s.length(); ++i) {
+            System.out.print(s.charAt(i) + " ");
+        }
+    }
+}
---
+++ src/test/java/ru/ssau/tk/name/practice/string/Task3_1Test.java
@@ -0,0 +1,12 @@
+package ru.ssau.tk.name.practice.string;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class Task3_1Test {
+    @Test
+    public void testPrintSymbols() {
+        Task3_1.printSymbols("Hello, world!");
+    }
+}
