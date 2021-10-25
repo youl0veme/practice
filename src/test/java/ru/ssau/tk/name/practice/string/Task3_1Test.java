a4f657f6fd5e9f28f7673be69afb0402f57800b6
Vechkanova <vechkanova.p@gmail.com>
2021-10-25 13:28:37 +0400

    Задание 3.1

--- src/test/java/ru/ssau/tk/name/practice/string/Task3_1Test.java
+++ src/test/java/ru/ssau/tk/name/practice/string/Task3_1Test.java
@@ -2,8 +2,6 @@ package ru.ssau.tk.name.practice.string;
 
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-
 public class Task3_1Test {
     @Test
     public void testPrintSymbols() {
