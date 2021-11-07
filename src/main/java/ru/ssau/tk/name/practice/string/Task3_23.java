a5428827b906ff0f70ed8a9852bbb864032e4649
Vechkanova <vechkanova.p@gmail.com>
2021-11-07 13:15:28 +0400

    Задание 3.23

---
+++ src/main/java/ru/ssau/tk/name/practice/string/Task3_23.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.name.practice.string;
+
+import static java.nio.charset.Charset.*;
+
+public class Task3_23 {
+    public static void main(String[] args) {
+        System.out.println(defaultCharset());
+    }
+}
